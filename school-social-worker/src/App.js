f47a0d4a1c60545419e3a5736b98edfe78d18643
Shawn Tompke <shawntompke@Shawns-MBP.routerlogin.net>
2020-02-03 10:42:23 -0800

    folders and components added to file structure

---
+++ school-social-worker/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>
+          School Social Worker
+        </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
