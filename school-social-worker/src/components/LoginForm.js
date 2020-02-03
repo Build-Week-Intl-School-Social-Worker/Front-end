ebc98c6ddc00c0c4c3b8fe64764254e5aa0e8a44
Michael Walter <Michael_Patrick.Walter@outlook.com>
2020-02-03 14:32:37 -0800

    LoginForm working, needs validations. ResisterForm in the works.

--- school-social-worker/src/components/LoginForm.js
+++ school-social-worker/src/components/LoginForm.js
@@ -1,23 +1,64 @@
 import React, { useState, useEffect } from 'react';
-import { withFormik, Form, Field } from 'formik';
+import { withFormik, Form, Field, Formik } from 'formik';
 import axios from 'axios';
+import * as Yup from 'yup';
 
-export const LoginForm = () => {
-    const [ credentials, setCredentials ] = setState({
-        email: '',
+export const LoginForm = ({values, errors, touched, status}) => {
+    const [ credentials, setCredentials ] = useState({
+        username: '',
         password: ''
     })
 
-    return (
+    // useEffect(() => {
+        
+    // })
 
-        <h2>Login:</h2> 
-        <div>
-            <Formik>
-                
-            </Formik>
-}
-        </div>
-    
+    return (   
+        
+            <Form>
+                <label htmlFor='username'>
+                    Username: 
+                    <Field name='username' type='text' placeHolder="Enter Username"/>
+                    {/* {touched.userName && errors.userName && (<p className="errors">{errors.userName}</p>)} */}
+                </label>
+
+                <label htmlFor="password">
+                    Password: 
+                    <Field name='password' type='password' placeHolder="Enter Password"/>
+                    {/* {touched.password && errors.password && (<p className="errors">{errors.password}</p>)} */}
+                </label>
+
+                <button type="submit">Login</button>
+
+            </Form>
+        
+        
+    );
 }
 
-export default LoginForm;
+const LoginSubmit = withFormik ({
+    mapPropsToValues(props) {
+        return {
+            email: props.username || "",
+            password: props.password || ""
+        };
+    },
+
+    // validationSchema: Yup.object().shape({
+    //     userName: Yup.string().required("Username is required!"),
+    //     userPassword: Yup.string().required("Password is required!")
+    // }),
+
+    handleSubmit( values, { setCredentials}) {
+        axios.post('https://regres.in/api/users', values)
+        .then ( response => {
+            console.log('Success', response);
+            setCredentials(response.data);
+            
+        })
+        .catch ( err => console.log('Error:', err));
+    }
+
+})(LoginForm)
+
+export default LoginSubmit;
--- school-social-worker/src/components/RegisterForm.js
+++ school-social-worker/src/components/RegisterForm.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { withFormik, Form, Field, Formik } from 'formik';
+import axios from 'axios';
+import * as Yup from 'yup';
 
 const RegisterForm = () => {
     
+    const  [ newUser, setNewUser ] = useState({
+        name: '',
+        email: '',
+        password: '',
+        status: ''
+    })
+
     return (
-        <>
-        <h2>Register Form Component</h2>
-        </>
+       <Form>
+
+            <label htmlFor='name'> 
+                <Field name='name' type='text' placeHolder='Enter Name' />
+            </label>
+
+            <label htmlFor='email'>
+                <Field name='email' type='email' placeHolder='Enter Email' />
+            </label>
+
+            <labelk htmlFor='password'>
+                <Field name='passowrd' type='password' placeHolder="Enter Password" />
+            </labelk>
+
+            <label htmlFor="status">
+                
+            </label>
+
+       </Form>
     )
 }
 
