1cc549ea9d5ae32d585799c706e7bfbf9b799126
Michael Walter <Michael_Patrick.Walter@outlook.com>
2020-02-03 16:38:47 -0800

    RegisterForm is working, and validation has been added to both pages.

--- school-social-worker/src/components/RegisterForm.js
+++ school-social-worker/src/components/RegisterForm.js
@@ -18,16 +18,25 @@ const RegisterForm = ({values, errors, touched, status}) => {
             <label htmlFor='name'> 
                 Name: 
                 <Field name='name' type='text' placeHolder='Enter Name' />
+                {touched.name && errors.name && (
+                    <p className="errors">{errors.name}</p>
+                )}
             </label>
 
             <label htmlFor='email'>
                 Email: 
                 <Field name='email' type='email' placeHolder='Enter Email' />
+                {touched.email && errors.email && (
+                    <p className="errors">{errors.email}</p>
+                )}
             </label>
 
             <labelk htmlFor='password'>
                 Password: 
-                <Field name='passowrd' type='password' placeHolder="Enter Password" />
+                <Field name='password' type='password' placeHolder="Enter Password" />
+                {touched.password && errors.password && (
+                    <p className="errors">{errors.password}</p>
+                )}
             </labelk>
 
             <label htmlFor="status">
@@ -37,6 +46,9 @@ const RegisterForm = ({values, errors, touched, status}) => {
                     <option value='admin'>Administrator</option>
                     <option value='worker'>Social Worker</option>
                 </Field>
+                {touched.select && errors.status && (
+                    <p className="errors">{errors.status}</p>
+                )}
             </label>
 
             <button type="submit">Register</button>
@@ -55,11 +67,20 @@ const RegisterSubmit = withFormik ({
         };
     },
 
-    handleSubmit( values, { setNewUser }) {
+    validationSchema: Yup.object().shape({
+        name: Yup.string().required("Name is required!"),
+        email: Yup.string().required("Email is required!"),
+        password: Yup.string().required("Password is required!"),
+        status: Yup.string().required("Role is required!")
+    }),
+
+    handleSubmit( values, { setNewUser, resetForm }) {
         axios.post('https://regres.in/api/users', values)
         .then ( response => {
             console.log('Success', response);
             setNewUser(response.data);
+            RegisterForm();
+
         })
         .catch ( err => console.log('Error on RegistrationForm: ', err));
     }
