97a7cca69afd878cc9762386889a459572a78380
Shawn Tompke <shawntompke@Shawns-MBP.routerlogin.net>
2020-02-03 16:51:07 -0800

    added styling to navigation

--- school-social-worker/src/components/Navigation.js
+++ school-social-worker/src/components/Navigation.js
@@ -11,6 +11,7 @@ color: white;
 background: #fff;
 padding: 0px;
 margin: 0px;
+
 `;
 
 const Logo = styled.img`
@@ -22,6 +23,10 @@ align-items: center;
 const LogoContainer = styled.div`
 display: flex;
 padding: 10px 80px;
+
+@media (max-width: 600px) {
+    display: none;
+  }
 `;
 
 const NavLinks = styled.div`
@@ -29,11 +34,18 @@ display: flex;
 width: 100%;
 justify-content: flex-end;
 align-items: center;
+font-size: 2.2rem;
+
+@media (max-width: 600px) {
+    justify-content: space-around;
+    font-size: 1.4rem;
+  }
 `;
 const Link = styled.div`
 padding: 10px;
 margin: 10px;
 color: #000;
+font-family: 'Manjari', sans-serif;
 `;
 
 
