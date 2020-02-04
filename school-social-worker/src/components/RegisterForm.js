53d99f483476b6a851fa017b7f02428c6355d043
Shawn Tompke <30378642+Stompke@users.noreply.github.com>
2020-02-04 10:48:04 -0800

    Update RegisterForm.js

--- school-social-worker/src/components/RegisterForm.js
+++ school-social-worker/src/components/RegisterForm.js
@@ -16,24 +16,64 @@ const RegisterForm = ({values, errors, touched, status}) => {
        <Form>
 
             <label htmlFor='name'> 
+                Name: 
                 <Field name='name' type='text' placeHolder='Enter Name' />
+                {touched.name && errors.name && (
+                    <p className="errors">{errors.name}</p>
+                )}
+            </label>
+
+            <label htmlFor='role_id'> 
+                role_id: 
+                <Field name='role_id' type='text' placeHolder='role_id' />
+                {/* {touched.name && errors.name && (
+                    <p className="errors">{errors.name}</p>
+                )} */}
+            </label>
+
+            <label htmlFor='phone'> 
+                phone: 
+                <Field name='phone' type='text' placeHolder='phone' />
+                {/* {touched.name && errors.name && (
+                    <p className="errors">{errors.name}</p>
+                )} */}
+            </label>
+
+            <label htmlFor='org_name'> 
+                org_name: 
+                <Field name='org_name' type='text' placeHolder='org_name' />
+                {/* {touched.name && errors.name && (
+                    <p className="errors">{errors.name}</p>
+                )} */}
             </label>
 
             <label htmlFor='email'>
+                Email: 
                 <Field name='email' type='email' placeHolder='Enter Email' />
+                {touched.email && errors.email && (
+                    <p className="errors">{errors.email}</p>
+                )}
             </label>
 
             <labelk htmlFor='password'>
-                <Field name='passowrd' type='password' placeHolder="Enter Password" />
+                Password: 
+                <Field name='password' type='password' placeHolder="Enter Password" />
+                {touched.password && errors.password && (
+                    <p className="errors">{errors.password}</p>
+                )}
             </labelk>
 
-            <label htmlFor="status">
+            {/* <label htmlFor="status">
+                Role: 
                 <Field as="select" name="select">
                     <option value='null'>Select</option>
                     <option value='admin'>Administrator</option>
                     <option value='worker'>Social Worker</option>
                 </Field>
-            </label>
+                {touched.select && errors.status && (
+                    <p className="errors">{errors.status}</p>
+                )}
+            </label> */}
 
             <button type="submit">Register</button>
 
@@ -47,15 +87,26 @@ const RegisterSubmit = withFormik ({
             name: props.name || "",
             email: props.email || "",
             password: props.password || "",
-            status: props.status || ""
+            role_id: props.role_id || "",
+            phone: props.phone || "",
+            org_name: props.org_name || ""
         };
     },
 
-    handleSubmit( values, { setNewUser }) {
-        axios.post('https://regres.in/api/users', values)
+    validationSchema: Yup.object().shape({
+        // name: Yup.string().required("Name is required!"),
+        // email: Yup.string().required("Email is required!"),
+        // password: Yup.string().required("Password is required!"),
+        // status: Yup.string().required("Role is required!")
+    }),
+
+    handleSubmit( values, { setNewUser, resetForm }) {
+        axios.post('https://school-social-worker.herokuapp.com/auth/register', values)
         .then ( response => {
             console.log('Success', response);
             setNewUser(response.data);
+            resetForm();
+
         })
         .catch ( err => console.log('Error on RegistrationForm: ', err));
     }
