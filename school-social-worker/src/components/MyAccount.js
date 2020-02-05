34f49281cfac4a1fd3381a8404372b8003327288
Shawn Tompke <shawntompke@Shawns-MBP.routerlogin.net>
2020-02-05 13:28:13 -0800

    data is displaying on MyAccount page

--- school-social-worker/src/components/MyAccount.js
+++ school-social-worker/src/components/MyAccount.js
@@ -3,31 +3,57 @@ import { connect } from "react-redux";
 import { axiosWithAuth } from '../utils/axiosWithAuth';
 import { loadUserData } from '../actions';
 
-const MyAccount = (props) => {
+const MyAccount =  (props) => {
     const [ userList, setUserList ] = useState([]);
+    const [ currentUser, setCurrentUser ] = useState({
+        id: null,
+        role_id: null,
+        name: "",
+        email: "",
+        phone: "",
+        password: "",
+        org_name: ""
+    })
 
-    useEffect(()=>{
+
+     useEffect(()=>{
         axiosWithAuth()
         .get('https://school-social-worker.herokuapp.com/api/users')
-        .then(res => {
-            setUserList(res.data)
+        .then(async res =>  {
+            console.log(res.data.find(item => item.email === props.email))
+            // let newData = await res.data.find(user => user.email === props.email)
+            // console.log(newData)
+            setCurrentUser(res.data.find(item => item.email === props.email))
+            //  setUserList(res.data)
         })
         .catch(err => {
             console.log(err)
         })
     },[])
+
+
     
 
     return (
         <>
         <h2>My Account Component</h2>
-        <h3>{props.email}</h3>
-        {userList.map(item => {
-            return (
-                <p>{item.name}</p>
-                
-        )
-        })}
+         <h3>{props.email}</h3> 
+        {/* {userList.map(item => {
+            return ( 
+                 <div>
+                    <p>{item.name}</p>
+                    <h2>Account type: {(item.role_id === 1) ? <span>Admin</span> : <span>Social Worker</span>}</h2>
+                    <p>{item.email}</p>
+                    <p>{item.phone}</p>
+                    <p>{item.org_name}</p>
+                </div> 
+         )
+        })}  */}
+        <h1>{currentUser.name}</h1>
+        <h2>Account type: {(currentUser.role_id === 1) ? <span>Admin</span> : <span>Social Worker</span>}</h2>
+        <h1>{currentUser.phone}</h1>
+        <h1>{currentUser.org_name}</h1>
+
         <button onClick={props.loadUserData}> click it</button>
         </>
     )
