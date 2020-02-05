a08b404a89d48e8d998aad543d1eff4a2d82d002
Shawn Tompke <shawntompke@Shawns-MBP.routerlogin.net>
2020-02-05 14:54:16 -0800

    doing changes on MyAccount

--- school-social-worker/src/components/MyAccount.js
+++ school-social-worker/src/components/MyAccount.js
@@ -2,57 +2,73 @@ import React, { useState, useEffect } from 'react';
 import { connect } from "react-redux";
 import { axiosWithAuth } from '../utils/axiosWithAuth';
 import { loadUserData } from '../actions';
+import { ScaleLoader } from "react-spinners";
+
+const initialState = {
+    id: 21,
+role_id: 1,
+name: "shawn initial state",
+email: "shawn@test.com",
+phone: "1231231234",
+password: "$2a$12$FxDqJN9GMfulDUUmhEARbe3OOiSbamZSRnMoZhuCYXBMG9BqCxw5a",
+org_name: "cashmere"
+}
 
 const MyAccount =  (props) => {
-    const [ userList, setUserList ] = useState([]);
-    const [ currentUser, setCurrentUser ] = useState({
-        id: null,
-        role_id: null,
-        name: "",
-        email: "",
-        phone: "",
-        password: "",
-        org_name: ""
-    })
+    const [ loading, setLoading ] = useState(false);
+    const [ currentUser, setCurrentUser ] = useState(initialState);
 
 
-     useEffect(()=>{
-        axiosWithAuth()
-        .get('https://school-social-worker.herokuapp.com/api/users')
-        .then(async res =>  {
-            console.log(res.data.find(item => item.email === props.email))
-            // let newData = await res.data.find(user => user.email === props.email)
-            // console.log(newData)
-            setCurrentUser(res.data.find(item => item.email === props.email))
-            //  setUserList(res.data)
-        })
-        .catch(err => {
-            console.log(err)
-        })
-    },[])
 
-
+            useEffect(()=>{
+                   setLoading(true)
+               axiosWithAuth()
+               .get('https://school-social-worker.herokuapp.com/api/users')
+               .then( res =>  {
+                   console.log(res.data.find(item => item.email === props.email))
+                   // let newData = await res.data.find(user => user.email === props.email)
+                   // console.log(newData)
+                    let newData = res.data.find(item => item.email === props.email)
+                   setCurrentUser(newData)
+                   //  setUserList(res.data)
+                   setLoading(false)
+                   
+               })
+               .catch(err => {
+                   setLoading(false)
+                   console.log(err)
+               })
+           },[])
     
 
-    return (
-        <>
-        <h2>My Account Component</h2>
-         <h3>{props.email}</h3> 
-        {/* {userList.map(item => {
-            return ( 
-                 <div>
-                    <p>{item.name}</p>
-                    <h2>Account type: {(item.role_id === 1) ? <span>Admin</span> : <span>Social Worker</span>}</h2>
-                    <p>{item.email}</p>
-                    <p>{item.phone}</p>
-                    <p>{item.org_name}</p>
-                </div> 
-         )
-        })}  */}
-        <h1>{currentUser.name}</h1>
-        <h2>Account type: {(currentUser.role_id === 1) ? <span>Admin</span> : <span>Social Worker</span>}</h2>
-        <h1>{currentUser.phone}</h1>
-        <h1>{currentUser.org_name}</h1>
+
+          
+          
+          
+          
+          return (
+              <>
+        <h1>My Account</h1>
+
+
+              {loading ?
+                  <ScaleLoader
+                  
+                  size={150}
+                  //size={"150px"} this also works
+                  color={"#123abc"}
+                  loading={props.isLoading}
+                  />
+              :
+                <>
+                    <h1>{currentUser.name}</h1>
+                    <h2>Account type: {(currentUser.role_id === 1) ? <span>Admin</span> : <span>Social Worker</span>}</h2>
+                    <h1>{currentUser.phone}</h1>
+                    <h1>{currentUser.org_name}</h1>
+                    </>
+          }
+
+
 
         <button onClick={props.loadUserData}> click it</button>
         </>
