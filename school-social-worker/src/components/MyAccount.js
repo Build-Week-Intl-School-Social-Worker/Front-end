94686bdec98186559a55aae4f9032dbee1be1005
Shawn Tompke <shawntompke@Shawns-MBP.routerlogin.net>
2020-02-05 12:39:54 -0800

    adding user data to MyAccount.js

--- school-social-worker/src/components/MyAccount.js
+++ school-social-worker/src/components/MyAccount.js
@@ -1,16 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { connect } from "react-redux";
-import axios from 'axios'
+import { axiosWithAuth } from '../utils/axiosWithAuth';
 import { loadUserData } from '../actions';
 
 const MyAccount = (props) => {
+    const [ userList, setUserList ] = useState([]);
 
+    useEffect(()=>{
+        axiosWithAuth()
+        .get('https://school-social-worker.herokuapp.com/api/users')
+        .then(res => {
+            setUserList(res.data)
+        })
+        .catch(err => {
+            console.log(err)
+        })
+    },[])
     
 
     return (
         <>
         <h2>My Account Component</h2>
         <h3>{props.email}</h3>
+        {userList.map(item => {
+            return (
+                <p>{item.name}</p>
+                
+        )
+        })}
         <button onClick={props.loadUserData}> click it</button>
         </>
     )
