11221e06a2b55df55ded2bfe1fcf40fe44c8c9eb
Shawn Tompke <shawntompke@Shawns-MBP.routerlogin.net>
2020-02-05 15:37:38 -0800

    edits to MyAccount

--- school-social-worker/src/components/MyAccount.js
+++ school-social-worker/src/components/MyAccount.js
@@ -3,6 +3,9 @@ import { connect } from "react-redux";
 import { axiosWithAuth } from '../utils/axiosWithAuth';
 import { loadUserData } from '../actions';
 import { ScaleLoader } from "react-spinners";
+import styled from 'styled-components';
+import profilePic from '../assets/profilepic.jpeg';
+
 
 const initialState = {
     id: 21,
@@ -20,37 +23,69 @@ const MyAccount =  (props) => {
 
 
 
-            useEffect(()=>{
-                   setLoading(true)
-               axiosWithAuth()
-               .get('https://school-social-worker.herokuapp.com/api/users')
-               .then( res =>  {
-                   console.log(res.data.find(item => item.email === props.email))
-                   // let newData = await res.data.find(user => user.email === props.email)
-                   // console.log(newData)
-                    let newData = res.data.find(item => item.email === props.email)
-                   setCurrentUser(newData)
-                   //  setUserList(res.data)
-                   setLoading(false)
-                   
-               })
-               .catch(err => {
-                   setLoading(false)
-                   console.log(err)
-               })
-           },[])
+        //     useEffect(()=>{
+        //            setLoading(true)
+        //        axiosWithAuth()
+        //        .get('https://school-social-worker.herokuapp.com/api/users')
+        //        .then( res =>  {
+        //            console.log(res.data.find(item => item.email === props.email))
+        //            // let newData = await res.data.find(user => user.email === props.email)
+        //            // console.log(newData)
+        //             let newData = res.data.find(item => item.email === props.email)
+        //            setCurrentUser(newData)
+        //            //  setUserList(res.data)
+        //            setLoading(false)
+        //        })
+        //        .catch(err => {
+        //            setLoading(false)
+        //            console.log(err)
+        //        })
+        //    },[])
     
+           const PageContainer = styled.div`
+           width: 100%;
+           display:flex;
+           justify-content: space-around;
+           flex-flow: row;
+           `;
 
-
-          
-          
+           const LeftSide = styled.div`
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            width: 50%;
+            background: #52abd8c9;
+            color: white;
+            padding: 0px;
+            margin: 50px;
+            border-radius: 10px;
+            `;
+           const RightSide = styled.div`
+            display: flex;
+            flex-direction: column;
+            width: 50%;
+            background: #52abd8c9;
+            color: white;
+            padding: 0px;
+            margin: 50px;
+            border-radius: 10px;
+            `;
+            
+            const ProfilePic = styled.img`
+            border-radius: 100%;
+            width: 100%;
+            width: 100px;
+            `;
+          const Line = styled.div`
+            display: flex;
+            width: 100%;
+            justify-content: space-between;
+            padding: 15px;
+          `;
           
           
           return (
               <>
-        <h1>My Account</h1>
-
-
               {loading ?
                   <ScaleLoader
                   
@@ -60,18 +95,31 @@ const MyAccount =  (props) => {
                   loading={props.isLoading}
                   />
               :
-                <>
-                    <h1>{currentUser.name}</h1>
-                    <h2>Account type: {(currentUser.role_id === 1) ? <span>Admin</span> : <span>Social Worker</span>}</h2>
-                    <h1>{currentUser.phone}</h1>
-                    <h1>{currentUser.org_name}</h1>
-                    </>
-          }
+              <PageContainer>
 
+            <LeftSide >
+                <h1>My Account</h1>
+                <ProfilePic src={profilePic} />
+                <Line><h4>Email: </h4><h3>{currentUser.email}</h3></Line>
+                
+            </LeftSide>
+            <RightSide >
+                
+                    <Line><h4>Name: </h4><h3>{currentUser.name}</h3></Line>
+                    <Line><h4>Account type: </h4>{(currentUser.role_id === 1) ? <h3>Admin</h3> : <h3>Social Worker</h3>}</Line>
+                    <Line><h4>Phone: </h4><h3>{currentUser.phone}</h3></Line>
+                    <Line><h4>Organization: </h4><h3>{currentUser.org_name}</h3></Line>
 
-
-        <button onClick={props.loadUserData}> click it</button>
-        </>
+                    
+                    
+                
+                <button onClick={props.loadUserData}> click it</button>
+                
+                </RightSide>
+                
+        </PageContainer>
+                }
+ </>
     )
 }
 
