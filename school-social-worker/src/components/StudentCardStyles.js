c60fa745a9ccd64973fc009f8ecda311f8e00fee
Michael Walter <Michael_Patrick.Walter@outlook.com>
2020-02-06 13:26:41 -0800

    Added snackbar to AddButton and DeleteButton

---
+++ school-social-worker/src/components/StudentCardStyles.js
@@ -0,0 +1,13 @@
+import  styled  from 'styled-components';
+
+export const ButtonBox = styled.div `
+    display: flex;
+    justify-content: space-between;
+    width: 90%;
+`
+
+export const AddEditBox = styled.div `
+    display: flex;
+    justify-content: space-around;
+    width: 40%;
+`
